99e61891b85be2d98d31f8573126deb308c6d5a9
ctoloczko <celina.toloczko@gmail.com>
2024-08-14 13:44:10 +0200

    added SearchEngine and CurrentWeather Components

---
+++ src/CurrentWeather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CurrentWeather() {
+  return (
+    <>
+      <h3>Wednesday</h3>
+      <h5>14 Aug 2024</h5>
+      <p>☀️</p>
+      <h2>25°C</h2>
+      <p>Warm, Sunny</p>
+      <p>💦 35%</p>
+      <p>💨 10km/h</p>
+    </>
+  );
+}
---
+++ src/SearchEngine.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SearchEngine() {
+  return (
+    <>
+      <form>
+        <input type="search" placeholder="Enter a city"></input>
+        <input type="submit" value="Search"></input>
+      </form>
+    </>
+  );
+}
