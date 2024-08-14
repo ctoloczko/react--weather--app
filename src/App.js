ade12999aa1a6cfc326be5cc74e833b35227bda5
ctoloczko <celina.toloczko@gmail.com>
2024-08-14 14:19:11 +0200

    added Forecast component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import SearchEngine from "./SearchEngine";
 import CurrentWeather from "./CurrentWeather";
+import Forecast from "./Forecast";
 
 function App() {
   return (
     <>
-      <SearchEngine />
-      <CurrentWeather />
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <SearchEngine />
+            <CurrentWeather />
+          </div>
+          <div class="col-9">
+            <Forecast />
+          </div>
+        </div>
+      </div>
+      <></>
     </>
   );
 }
---
+++ src/Forecast.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function Forecast() {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col border border-dark ">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+          <div class="col border border-dark ">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+          <div class="col border border-dark">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+          <div class="col border border-dark">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+          <div class="col border border-dark">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+          <div class="col border border-dark">
+            <h3>Thu</h3>
+            <h3>15 Aug</h3>
+            <h2>🌤️</h2>
+            <p>22°C</p>
+            <p>15°C</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
